3890da73d4e1f9485f2d6a6e696a888c4275b589
huceke <gimli>
2012-10-10 13:38:31 +0200

    [xbmc] libcec revision bump

--- package/xbmc/libcec/libcec.mk
+++ package/xbmc/libcec/libcec.mk
@@ -3,7 +3,7 @@
 # libcec
 #
 #############################################################
-LIBCEC_VERSION = 8b0462ea5e37459446a493c124e4c2d7a9e6b108
+LIBCEC_VERSION = a6dadc0a57508ad6473b35c5d989d5e14f5ce89a
 LIBCEC_SITE = git://github.com/Pulse-Eight/libcec.git
 LIBCEC_INSTALL_STAGING = YES
 LIBCEC_INSTALL_TARGET = YES
