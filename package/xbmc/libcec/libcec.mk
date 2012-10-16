d6cb9a23f6c179f6828f9e21c61a7b7f4b940e0d
huceke <gimli>
2012-10-16 11:38:14 +0200

    [libcec] revision bump and silence : the mangling of 'va_list' has changed in GCC 4.4 warnings

--- package/xbmc/libcec/libcec.mk
+++ package/xbmc/libcec/libcec.mk
@@ -3,14 +3,15 @@
 # libcec
 #
 #############################################################
-LIBCEC_VERSION = a6dadc0a57508ad6473b35c5d989d5e14f5ce89a
+LIBCEC_VERSION = a9ac151c8265f1d241799f7d6beaf4e13d4066ca
 LIBCEC_SITE = git://github.com/Pulse-Eight/libcec.git
 LIBCEC_INSTALL_STAGING = YES
 LIBCEC_INSTALL_TARGET = YES
 LIBCEC_AUTORECONF = YES
 LIBCEC_CONF_OPT = --enable-rpi \
 									--with-rpi-include-path=$(STAGING_DIR)/opt/vc/include \
-									--with-rpi-lib-path=$(STAGING_DIR)/opt/vc/lib
+									--with-rpi-lib-path=$(STAGING_DIR)/opt/vc/lib \
+									CFLAGS="$(TARGET_CFLAGS) -Wno-psabi" CXXFLAGS="$(TARGET_CXXFLAGS) -Wno-psabi"
 LIBCEC_DEPENDENCIES = lockdev
 
 $(eval $(call AUTOTARGETS))
