23725052cfc212fabc7fc62315c2b1f4a3d263ab
gimli <ebsi4711@gmail.com>
2012-11-21 17:36:47 +0100

    [allwinner] changed fetch and use egl mali sdk headers

--- package/allwinner-mali/allwinner-mali.mk
+++ package/allwinner-mali/allwinner-mali.mk
@@ -11,12 +11,22 @@ ALLWINNER_MALI_INSTALL_TARGET = YES
 define ALLWINNER_MALI_INSTALL_STAGING_CMDS
 	$(MAKE) -C $(@D) VERSION=r3p0 ABI=armhf prefix=$(STAGING_DIR)/usr/ framebuffer
 	$(MAKE) -C $(@D) prefix=$(STAGING_DIR)/usr/ headers
-	install -m 666 package/allwinner-mali/eglplatform.h $(STAGING_DIR)/usr/include/EGL/
-	install -m 666 package/allwinner-mali/fbdev_window.h $(STAGING_DIR)/usr/include/EGL/
+	install -m 666 $(@D)/Mali_OpenGL_ES_2.0_SDK_for_Linux_On_ARM_v1.2.0/simple-framework/inc/mali/EGL/fbdev_window.h \
+		 $(STAGING_DIR)/usr/include/EGL/
+	install -m 666 $(@D)/Mali_OpenGL_ES_2.0_SDK_for_Linux_On_ARM_v1.2.0/inc/EGL/eglplatform.h \
+		 $(STAGING_DIR)/usr/include/EGL/
+	install -m 666 $(@D)/Mali_OpenGL_ES_2.0_SDK_for_Linux_On_ARM_v1.2.0/inc/EGL/eglext.h \
+		 $(STAGING_DIR)/usr/include/EGL/
 endef
 
 define ALLWINNER_MALI_INSTALL_TARGET_CMDS
 	$(MAKE) -C $(@D) VERSION=r3p0 ABI=armhf prefix=$(TARGET_DIR)/usr/ framebuffer
+endef
+
+define ALLWINNER_MALI_BUILD_CMDS
+	wget -c http://malideveloper.com/files/Mali_OpenGL_ES_2.0_SDK_for_Linux_On_ARM_v1.2.0.9310_Linux.tar.gz \
+			-O dl/Mali_OpenGL_ES_2.0_SDK_for_Linux_On_ARM_v1.2.0.9310_Linux.tar.gz
+	tar -xzvf dl/Mali_OpenGL_ES_2.0_SDK_for_Linux_On_ARM_v1.2.0.9310_Linux.tar.gz -C $(@D)
 endef
 
 $(eval $(call GENTARGETS))
