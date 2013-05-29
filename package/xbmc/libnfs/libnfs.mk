49c1c4072baed62fb2ab9fab258da1b97b989bf5
huceke <gimli>
2013-05-29 14:21:31 +0200

    [libnfs] revision bump

--- package/xbmc/libnfs/libnfs.mk
+++ package/xbmc/libnfs/libnfs.mk
@@ -3,7 +3,7 @@
 # libnfs
 #
 #############################################################
-LIBNFS_VERSION = 47e5a2c74007f57751ebe9054669404eeb2fe948
+LIBNFS_VERSION = c5e346e15c043a6253cc5a838dd9c77febf624aa
 LIBNFS_SITE = git://github.com/sahlberg/libnfs.git
 LIBNFS_INSTALL_STAGING = YES
 LIBNFS_INSTALL_TARGET = YES
